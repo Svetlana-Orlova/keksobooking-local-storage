8266b5dd09e2377dccce0ae40d6402db25776667
Svetlana-Orlova <Svetlana-Orlova@github.com>
2020-12-15 20:10:45 +0300

    Удаление глобальных переменных в localStorage.js

--- bundle.js
+++ bundle.js
@@ -124,11 +124,9 @@ window.util = {
 (() => {
 
 
-let dataOfLocalStorage = window.util.ADS;
-let store = [];
-
 const loadData = () => {
   let storageEmpty = true;
+  let store = [];
   try {
     if (localStorage.getItem(`keksobooking_local_data`) !== null) {
       storageEmpty = false;
@@ -139,7 +137,7 @@ const loadData = () => {
   if (!storageEmpty) {
     store = JSON.parse(localStorage[`keksobooking_local_data`]);
   } else {
-    let jsonArray = JSON.stringify(dataOfLocalStorage);
+    let jsonArray = JSON.stringify(window.util.ADS);
     localStorage.setItem(`keksobooking_local_data`, jsonArray);
     store = JSON.parse(localStorage[`keksobooking_local_data`]);
   }
@@ -147,7 +145,7 @@ const loadData = () => {
 };
 
 const uploadData = (ad) => {
-  store = JSON.parse(localStorage[`keksobooking_local_data`]);
+  let store = JSON.parse(localStorage[`keksobooking_local_data`]);
   store.push(ad);
   let jsonArrayOfAds = JSON.stringify(store);
   localStorage.setItem(`keksobooking_local_data`, jsonArrayOfAds);
--- js/localStorage.js
+++ js/localStorage.js
@@ -1,10 +1,8 @@
 'use strict';
 
-let dataOfLocalStorage = window.util.ADS;
-let store = [];
-
 const loadData = () => {
   let storageEmpty = true;
+  let store = [];
   try {
     if (localStorage.getItem(`keksobooking_local_data`) !== null) {
       storageEmpty = false;
@@ -15,7 +13,7 @@ const loadData = () => {
   if (!storageEmpty) {
     store = JSON.parse(localStorage[`keksobooking_local_data`]);
   } else {
-    let jsonArray = JSON.stringify(dataOfLocalStorage);
+    let jsonArray = JSON.stringify(window.util.ADS);
     localStorage.setItem(`keksobooking_local_data`, jsonArray);
     store = JSON.parse(localStorage[`keksobooking_local_data`]);
   }
@@ -23,7 +21,7 @@ const loadData = () => {
 };
 
 const uploadData = (ad) => {
-  store = JSON.parse(localStorage[`keksobooking_local_data`]);
+  let store = JSON.parse(localStorage[`keksobooking_local_data`]);
   store.push(ad);
   let jsonArrayOfAds = JSON.stringify(store);
   localStorage.setItem(`keksobooking_local_data`, jsonArrayOfAds);
